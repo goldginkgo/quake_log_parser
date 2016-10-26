b2d0523fc0e3ca0aca5b56e6c6894503926a60ba
Frank Dai <yongjindai09@gmail.com>
2016-10-26 00:53:55 +0000

    [Task 3] Fix for display information

--- main.rb
+++ main.rb
@@ -2,5 +2,7 @@ require_relative 'lib/quake_log_parser'
 
 parser = QuakeLogParser.new(ARGV[0])
 parser.parse_log_file
+puts "====================     Task 1 and Task 2     ===================="
 parser.display_game_information
+puts "====================          Task 3           ===================="
 parser.display_aggregation_kill_reasons
