93f54effe242dc66304d6779bd3416ffb6a75e24
Frank Dai <yongjindai09@gmail.com>
2016-10-26 00:59:01 +0000

    [Task 3] Fix for ARGV

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/quake_log_parser'
 
-parser = QuakeLogParser.new(ARGV[0])
+parser = ARGV[0] ? QuakeLogParser.new(ARGV[0]) : QuakeLogParser.new
 parser.parse_log_file
 puts "====================     Task 1 and Task 2     ===================="
 parser.display_game_information
